a139f28127877b72918cfce2049999ec771c8caf
ravi kushwah <proficientrvkmr@gmail.com>
2020-01-27 13:41:52 +0530

    renaming

--- POC_CoreJava2/src/main/java/com/java/practice/collection/QueueDemo.java
+++
@@ -1,9 +0,0 @@
-package com.java.practice.collection;
-
-public class QueueDemo {
-    public static void main(String[] args) {
-//        Queue<Integer> q = new LinkedListExample<>();
-
-
-    }
-}
